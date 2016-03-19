9c19c0fda7500bf26709939de830c419b922bff2
mliu16 <ming.liutianyou@gmail.com>
2016-03-19 00:24:39 -0500

    Add Instance and Constructor.

--- src/colormandelbrot/ColorMandelbrot.java
+++ src/colormandelbrot/ColorMandelbrot.java
@@ -1,25 +1,70 @@
 package colormandelbrot;
 
- /******************************************************************************
- *  Compilation:  javac ColorMandelbrot.java
- *  Execution:    java Mandelbrot xmid ymid size < colors.txt
- *  Dependencies: Picture.java StdIn.java
- *
- *  Plots the Mandelbrot set in color.
- *
- *  % java ColorMandelbrot -.5 0 2
- *
- *  // increase dwell
- *  % java ColorMandelbrot -0.7615134027775 0.0794865972225 0.0032285925920
- *
- ******************************************************************************/
 
 import edu.princeton.cs.Picture;
 import java.awt.Color;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class ColorMandelbrot {
+public class ColorMandelbrot extends JFrame implements ActionListener  {
     
+    private final JPanel ctrlPanel;
+    private final JPanel btnPanel;
+    private final int numIter = 50;
+    private final double zoom = 130;
+    private final double zoomIncrease = 100;
+    private final int ITERS = 256;
+    private BufferedImage I;
+    private double zx, zy, xc, yc, temp;
+    private final int xMove, yMove = 0;
+    private final JButton[] ctrlBtns = new JButton[9];
+    private final Color themeColor = new Color(150, 180, 200);
     
+    public ColorMandelbrot (){
+         super("Mandelbrot Set");
+        setBounds(100, 100, 800, 600);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        plotPoints();
+
+        Container contentPane = getContentPane();
+
+        contentPane.setLayout(null);
+
+        ctrlPanel = new JPanel();
+        ctrlPanel.setBounds(600, 0, 200, 600);
+        ctrlPanel.setBackground(themeColor);
+        ctrlPanel.setLayout(null);
+
+        btnPanel = new JPanel();
+        btnPanel.setBounds(0, 200, 200, 200);
+        btnPanel.setLayout(new GridLayout(3, 3));
+        btnPanel.setBackground(themeColor);
+
+        ctrlBtns[1] = new JButton("up");
+        ctrlBtns[7] = new JButton("down");
+        ctrlBtns[3] = new JButton("left");
+        ctrlBtns[5] = new JButton("right");
+        ctrlBtns[2] = new JButton("+");
+        ctrlBtns[0] = new JButton("-");
+        ctrlBtns[8] = new JButton(">");
+        ctrlBtns[6] = new JButton("<");
+        ctrlBtns[4] = new JButton();
+
+        contentPane.add(ctrlPanel);
+        contentPane.add(new imgPanel());
+        ctrlPanel.add(btnPanel);
+
+        for (int x = 0; x < ctrlBtns.length; x++) {
+            btnPanel.add(ctrlBtns[x]);
+            ctrlBtns[x].addActionListener(this);
+        }//for
+
+        validate();
+
+    }
     // return number of iterations to check if c = a + ib is in Mandelbrot set
     public static int mand(Complex z0, int d) {
         Complex z = z0;
@@ -30,16 +75,11 @@ public class ColorMandelbrot {
         return d;
     }
     
-    public void drawPoints(){
-        double xc   = 0.0;
-        double yc   = 0.0;
+    public void plotPoints(){
+        xc   = 0.0;
+        yc   = 0.0;
         double size = 2;
-
-        int N = 512;
-
-        int ITERS = 256;
-
-
+        int N = 512;       
         // read in color map
         Color[] colors = new Color[ITERS];
         for (int t = 0; t < ITERS; t++) {
@@ -47,9 +87,7 @@ public class ColorMandelbrot {
             int g = (int) (1 + Math.random()*254);
             int b = (int) (1 + Math.random()*254);
             colors[t] = new Color(r, g, b);
-        }
-
-
+        }//for
         // compute Mandelbrot set  
         Picture pic = new Picture(N, N);
         for (int i = 0; i < N; i++) {
@@ -59,14 +97,19 @@ public class ColorMandelbrot {
                 Complex z0 = new Complex(x, y);
                 int t = mand(z0, ITERS - 1);
                 pic.set(i, N-1-j, colors[t]);
-            }
-        }
+            }//for
+        }//for
         pic.show();
     }//drawPoints
     
     public static void main(String[] args)  {
         ColorMandelbrot colorMandelbrot = new ColorMandelbrot();
-        colorMandelbrot.drawPoints();
+        colorMandelbrot.plotPoints();
     }//main ( String, args )
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }//ColorMandelbrot

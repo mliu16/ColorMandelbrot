a2bb03328428f3cc79ab55ebaee0f05f0823b3cd
mliu16 <ming.liutianyou@gmail.com>
2016-03-19 00:27:10 -0500

    Add imgPanel class.

---
+++ src/colormandelbrot/imgPanel.java
@@ -0,0 +1,15 @@
+
+package colormandelbrot;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Ling
+ */
+public class imgPanel extends JPanel {
+
+        public imgPanel() {
+            setBounds(0, 0, 600, 600);
+        }//imgPanel()
+}

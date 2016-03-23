81d919fcd1eee83c3ff03ea31621fad411d38663
mliu16 <ming.liutianyou@gmail.com>
2016-03-23 09:30:27 -0500

    Add control button and panel.

--- src/colormandelbrot/ColorMandelbrot.java
+++ src/colormandelbrot/ColorMandelbrot.java
@@ -1,5 +1,25 @@
+/**
+ * Copyright (c) <2016> <copyright Ming Liu>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package colormandelbrot;
-
 
 import edu.princeton.cs.Picture;
 import java.awt.Color;
@@ -9,8 +29,14 @@ import javax.swing.*;
 import java.awt.event.*;
 import static javafx.scene.input.KeyCode.I;
 
-public class ColorMandelbrot extends JFrame implements ActionListener  {
-    
+/**
+ *
+ * @author Ming Liu
+ * @version March 22 2016
+ */
+public class ColorMandelbrot extends JFrame implements ActionListener {
+
+    //instance
     private final JPanel ctrlPanel;
     private final JPanel btnPanel;
     private final int numIter = 50;
@@ -23,8 +49,11 @@ public class ColorMandelbrot extends JFrame implements ActionListener  {
     private int yMove = 0;
     private final JButton[] ctrlBtns = new JButton[9];
     private final Color themeColor = new Color(150, 180, 200);
-    
-    public ColorMandelbrot (){
+
+    /**
+     * Constructor
+     */
+    public ColorMandelbrot() {
         super("Mandelbrot Set");
         setBounds(100, 100, 800, 600);
         setResizable(false);
@@ -35,26 +64,30 @@ public class ColorMandelbrot extends JFrame implements ActionListener  {
 
         contentPane.setLayout(null);
 
+        //Set up control Panel
         ctrlPanel = new JPanel();
         ctrlPanel.setBounds(600, 0, 200, 600);
-        ctrlPanel.setBackground(themeColor);
+        ctrlPanel.setBackground(Color.GREEN);
         ctrlPanel.setLayout(null);
 
+        //Set up button Panel
         btnPanel = new JPanel();
         btnPanel.setBounds(0, 200, 200, 200);
-        btnPanel.setLayout(new GridLayout(3, 3));
-        btnPanel.setBackground(themeColor);
+        btnPanel.setLayout(new GridLayout(4, 4));
+        btnPanel.setBackground(Color.GRAY);
 
-        ctrlBtns[1] = new JButton("up");
-        ctrlBtns[7] = new JButton("down");
-        ctrlBtns[3] = new JButton("left");
-        ctrlBtns[5] = new JButton("right");
-        ctrlBtns[2] = new JButton("+");
-        ctrlBtns[0] = new JButton("-");
-        ctrlBtns[8] = new JButton(">");
-        ctrlBtns[6] = new JButton("<");
-        ctrlBtns[4] = new JButton();
+        //Create buttons
+        ctrlBtns[1] = new JButton("Up");
+        ctrlBtns[7] = new JButton("Down");
+        ctrlBtns[3] = new JButton("Left");
+        ctrlBtns[5] = new JButton("Right");
+        ctrlBtns[0] = new JButton("+");
+        ctrlBtns[2] = new JButton("-");
+        ctrlBtns[6] = new JButton(">");
+        ctrlBtns[8] = new JButton("<");
+        ctrlBtns[4] = new JButton("@");
 
+        //add control and button panel to content Panel
         contentPane.add(ctrlPanel);
         contentPane.add(new imgPanel());
         ctrlPanel.add(btnPanel);
@@ -65,30 +98,34 @@ public class ColorMandelbrot extends JFrame implements ActionListener  {
         }//for
 
         validate();
-    }
-    
+    } // ColorMandelbrot
+
     public class imgPanel extends JPanel {
 
         public imgPanel() {
             setBounds(0, 0, 600, 600);
-        }//imgPanel()  
-         public void paint(Graphics g) {
+        } //imgPanel()  
+
+        public void paint(Graphics g) {
             super.paint(g);
             g.drawImage(I, 0, 0, this);
-        }//paint()
-       }
-    
+        } //paint()
+    } //imgPanel
+
     // return number of iterations to check if c = a + ib is in Mandelbrot set
     public static int mand(Complex z0, int d) {
         Complex z = z0;
         for (int t = 0; t < d; t++) {
-            if (z.abs() > 2.0) return t;
+            if (z.abs() > 2.0) {
+                return t;
+            } // if
             z = z.times(z).plus(z0);
-        }
+        } // for
         return d;
-    }
-    
- public void plotPoints() {
+    } // mand
+
+    //Modified from Complex
+    public void plotPoints() {
         I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
         for (int y = 0; y < getHeight(); y++) {
             for (int x = 0; x < getWidth(); x++) {
@@ -107,32 +144,35 @@ public class ColorMandelbrot extends JFrame implements ActionListener  {
         }//for
     }//plotPoints
     
+    
+    //Create Action Events
+    @Override
     public void actionPerformed(ActionEvent ae) {
         String event = ae.getActionCommand();
 
         switch (event) {
-            case "up":
-                yMove -= 100;
+            case "Up":
+                yMove -= 110;
                 break;
-            case "down":
-                yMove += 100;
+            case "Down":
+                yMove += 110;
                 break;
-            case "left":
-                xMove -= 100;
+            case "Left":
+                xMove -= 110;
                 break;
-            case "right":
-                xMove += 100;
-                break;            
+            case "Right":
+                xMove += 110;
+                break;
             case "+":
                 double initialZoom = zoom;
                 zoom += zoomIncrease;
-                zoomIncrease *= 2;
-                xMove *= 2;
-                yMove *= 2;
+                zoomIncrease *=2;
+                xMove *=2;
+                yMove *=2;
                 break;
             case "-":
                 zoom -= zoomIncrease;
-                zoomIncrease -= 100;
+                zoomIncrease -= 110;
                 break;
             case ">":
                 ITERS++;
@@ -145,12 +185,12 @@ public class ColorMandelbrot extends JFrame implements ActionListener  {
         plotPoints();
         validate();
         repaint();
-    }//actionPerfomed
-    
-    public static void main(String[] args)  {
+    } //actionPerfomed
+
+    public static void main(String[] args) {
 //        ColorMandelbrot colorMandelbrot = new ColorMandelbrot();
 //        colorMandelbrot.plotPoints();
-          new ColorMandelbrot().setVisible(true);
+        new ColorMandelbrot().setVisible(true);
     }//main ( String, args )    
 
 }//ColorMandelbrot
